6a8aae7c9a2dceafe352652f07e1d024bf0995be
Marlonfpereira <marlon.fa.pereira@hotmail.com>
2023-11-06 19:47:27 -0300

    Results

--- testes.py
+++ testes.py
@@ -1,30 +1,7 @@
 import subprocess 
 import time
 
-for _ in range(1):
-
-    # Obtenha o tempo de início
-    tempo_inicial = time.time()
-    
-    result = subprocess.run(["time", "-p", "./restaurant-serial.x", "20"] , capture_output=True).stderr
-    result = result.decode("utf-8")
-    # Obtenha o tempo de término
-    tempo_final = time.time()
-
-    # Calcule o tempo decorrido
-    tempo_decorrido = tempo_final - tempo_inicial
-
-    real_time = float(result.split("user")[0].split("real")[1])
-    user_time = float(result.split("sys")[0].split("user")[1])
-    sys_time = float(result.split("sys")[1])
-
-    
-    with open("results-serial.txt", "a+") as result_file:
-        result_file.write(f"{real_time},{user_time},{sys_time}\n")
-
-
-
-for _ in range(1):
+for _ in range(10):
 
     # Obtenha o tempo de início
     tempo_inicial = time.time()
@@ -45,13 +22,34 @@ for _ in range(1):
         result_file.write(f"{real_time},{user_time},{sys_time}\n")
 
 
+for _ in range(10):
+
+    # Obtenha o tempo de início
+    tempo_inicial = time.time()
+    
+    result = subprocess.run(["time", "-p", "./restaurant-serial.x", "20"] , capture_output=True).stderr
+    result = result.decode("utf-8")
+    # Obtenha o tempo de término
+    tempo_final = time.time()
+
+    # Calcule o tempo decorrido
+    tempo_decorrido = tempo_final - tempo_inicial
+
+    real_time = float(result.split("user")[0].split("real")[1])
+    user_time = float(result.split("sys")[0].split("user")[1])
+    sys_time = float(result.split("sys")[1])
+
+    with open("results-serial.txt", "a+") as result_file:
+        result_file.write(f"{real_time},{user_time},{sys_time}\n")
+
+
 for kitchens in range(1,11):
     for waiters in range(1,11):
 
         # Obtenha o tempo de início
         tempo_inicial = time.time()
         
-        result = subprocess.run([f"time", "-p", "./restaurant-fixed.x", "20", {kitchens}, {waiters}] , capture_output=True).stderr
+        result = subprocess.run(["time", "-p", "./restaurant-fixed.x", "20", f"{kitchens}", f"{waiters}"] , capture_output=True).stderr
         result = result.decode("utf-8")
         # Obtenha o tempo de término
         tempo_final = time.time()
